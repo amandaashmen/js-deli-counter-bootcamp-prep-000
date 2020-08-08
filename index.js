641a27858ef60d91520288a7f8a4dc73c62dccd9
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 15:46:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function currentLine(line) {
     return print + " empty."
   }
   else {
+    print += ": "
     for(let i = 0; i < line.length; i++) {
-      print += ":" + (i+1) + ". " + line[i]
+      print += (i+1) + ". " + line[i]
       if (i !== line.length-1)
         print += ", "
     }
