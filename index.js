55355eccbdbba14a094713c7e6743b1cd5c6c899
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 15:36:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length + 1} in line`
 }
 
-console.log(takeANumber(['a'], 'dog'))
+function nowServing(line) {
+  if (line.length > 0) {
+  var serve = line[0]
+  line.shift()
+  return serve
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
