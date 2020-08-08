4b21fe81669938c38ac93dd9421178baac6e90c1
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 15:41:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(line) {
+  var print = ""
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    for(let i = 0; i < line.length; i++) {
+      print += i + line[i]
+    }
+  }
+}
+
+currentLine(["a", "b"])
