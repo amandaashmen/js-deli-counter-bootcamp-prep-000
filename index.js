26ea8872ebe03f00ed29c7c86730be3f8efe3f02
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 15:43:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  var print = ""
+  var print = "The line is currently"
   if (line.length === 0) {
-    return "The line is currently empty."
+    return print + "empty."
   }
   else {
     for(let i = 0; i < line.length; i++) {
-      print += (i+1) + line[i]
+      print += (i+1) + "." + line[i]
     }
   }
   return print
